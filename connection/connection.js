e3113eda25cf4657579b91f2786f6e9612713a21
chirag talpada <99867142+chirag-talpada@users.noreply.github.com>
2023-03-28 17:51:28 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 const connection = mysql.createPool(
     {
-        host: 'localhost',
+        host: 'sql12.freemysqlhosting.net',
         user: process.env.DB_USER,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD
